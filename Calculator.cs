81fd305698abfe8ff23c6c5088795c8b5b7a196e
imorrell <imorrell@ltu.edu>
2019-09-06 18:44:43 -0400

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConverter
+{
+    class Calculator
+    {
+        //create variables to perform calculations
+
+        double numOne;
+        double numTwo;
+        double numThree;
+
+        double total;
+        double average;
+        double high;
+        double low;
+
+        //create a constructor that inherits the three numbers
+        public Calculator(double one, double two, double three)
+        {
+            //assign each number in parameter to class variable
+
+            numOne = one;
+            numTwo = two;
+            numThree = three;
+        }
+
+        //create a function to calculate the total
+        public double calTotal()
+        {
+          total = numOne + numTwo + numThree;
+
+            return total;
+        }
+
+        //create a function to calculate the average
+        public double calAverage()
+        {
+            average = total/3;
+
+            return average;
+        }
+
+        //this function determines which number is the highest
+        public double calHigh()
+        {
+
+            if (numOne > numTwo && numOne > numThree)
+            {
+                high = numOne;
+                return high;
+            }
+            else if (numTwo > numOne && numTwo > numThree)
+            {
+
+                high = numTwo;
+            }
+            else
+            {
+
+                high = numThree;
+            }
+
+            return high;
+        }
+
+        //This function determine which number is the lowest
+        public double calLow()
+        {
+            if (numOne < numTwo && numOne < numThree)
+            {
+                low = numOne;
+            }
+            else if (numTwo < numOne && numTwo < numThree)
+            {
+
+                low = numTwo;
+            }
+            else
+            {
+
+                low = numThree;
+            }
+
+            return low;
+        }
+
+        //this function is used to display the the different calculations from methods above
+        public void displayCal()
+        {
+            Console.WriteLine("Total: " + total);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Average: " + average);
+
+            Console.WriteLine();
+
+            Console.WriteLine("High: " + high);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Low: " + low);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿//Programmer: Ivoire Morrell
+//Program Name: Currency Format Converter
+//Date: September 5, 2019
+// Description: The CUrrency format converter takes three different integers entered by the user, 
+//calculates the total, average, and the hugh/low from these numbers then displays the total in
+//four different currencies 
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create varibles the will be used for user input and to calculate total, average
+            double numOne;
+            double numTwo;
+            double numThree;
+
+            double total;
+            double average;
+            double high;
+            double low;
+
+            //welcome the user to the program and ask them to enter three numbers
+
+            Console.WriteLine("Welcome to the Currency format converter! please enter a number");
+
+            numOne = Convert.ToDouble(Console.ReadLine());
+
+            //used to make a space
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a second number");
+
+            numTwo = Convert.ToDouble(Console.ReadLine());
+
+            //used to make a space
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a third number");
+
+            numThree = Convert.ToDouble(Console.ReadLine());
+
+            //used to make a space
+            Console.WriteLine();
+
+            //create a caluculator class object to calculate total, average, and high/low
+            Calculator cali = new Calculator(numOne, numTwo, numThree);
+
+            //calculate the total of these three numbers
+            total = cali.calTotal();
+
+            //Calculate the average 
+            average = cali.calAverage();
+
+            //determine which number is the highest
+            high = cali.calHigh();
+
+            //Detrmine which number is the lowest
+            low = cali.calLow();
+
+            //display the total, average, high, and low
+            cali.displayCal();
+
+            //print and display total in US
+            Console.WriteLine(total.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine();
+
+            //print and display total in Swedish
+            Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")));
+
+            Console.WriteLine();
+
+            //print and display total in Japanese
+            Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+
+            Console.WriteLine();
+
+            //print and display total in Thai 
+            Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
